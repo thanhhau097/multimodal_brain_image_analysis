50c581fd1f12abf5e56cb498c9e7b54e74e2a052
Nguyen Thanh Hau <thanhhau097@gmail.com>
2020-02-22 23:01:26 +1100

    add sync batch norm

---
+++ models/unet.py
@@ -0,0 +1,9 @@
+# from torch import nn
+# from torch.nn import functional as F
+# import math
+# import torch
+#
+# # adapt from https://github.com/MIC-DKFZ/BraTS2017
+#
+# def normalization(planes, norm='bn'):
+#
