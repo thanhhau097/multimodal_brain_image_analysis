d75af1ae2699b189eaaeb0a75d077d9af77c0bcf
Nguyen Thanh Hau <thanhhau097@gmail.com>
2020-02-23 23:06:43 +1100

    update base code

--- models/unet.py
+++
@@ -1,9 +0,0 @@
-# from torch import nn
-# from torch.nn import functional as F
-# import math
-# import torch
-#
-# # adapt from https://github.com/MIC-DKFZ/BraTS2017
-#
-# def normalization(planes, norm='bn'):
-#
